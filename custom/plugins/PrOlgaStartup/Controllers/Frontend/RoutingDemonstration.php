453dd8cc0077d57ac47ee7141f8d2bdf7d8b4755
pr-olga <frkhorkovaolga@gmail.com>
2018-11-18 17:04:24 +0100

    feat: Add first controller'
    Add action which is accessible under same url

---
+++ custom/plugins/PrOlgaStartup/Controllers/Frontend/RoutingDemonstration.php
@@ -0,0 +1,13 @@
+<?php
+
+class Shopware_Controllers_Frontend_RoutingDemonstration extends Enlight_Controller_Action{
+
+    // set url frontend/routingDemonstration
+
+    public function indexAction(){
+
+        die("Ich war hier!");
+
+    }
+
+}
