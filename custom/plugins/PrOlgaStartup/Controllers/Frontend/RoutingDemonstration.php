4d9f9ceef9ecb9d7c232206ffedb4ff9e2cd92d2
pr-olga <frkhorkovaolga@gmail.com>
2018-11-18 17:19:16 +0100

    feat: Add and register a custom template for plugin
    Add preDispatch function

--- custom/plugins/PrOlgaStartup/Controllers/Frontend/RoutingDemonstration.php
+++ custom/plugins/PrOlgaStartup/Controllers/Frontend/RoutingDemonstration.php
@@ -2,11 +2,18 @@
 
 class Shopware_Controllers_Frontend_RoutingDemonstration extends Enlight_Controller_Action{
 
+    // register a directory
+    public function preDispatch () {
+
+        $this -> view -> addTemplateDir(__DIR__ . '/../../Resources/views');
+
+    }
+
+
     // set url frontend/routingDemonstration
 
-    public function indexAction(){
+    public function indexAction () {
 
-        die("Ich war hier!");
 
     }
 
---
+++ custom/plugins/PrOlgaStartup/Resources/views/frontend/routing_demonstration/index.tpl
@@ -0,0 +1,1 @@
+<h1>Hallo! Ich bin ein Template!!</h1>
